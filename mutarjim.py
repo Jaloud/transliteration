62ff5661926a964ea6399f316a0ec54b403bd2f1
Jaloud <github@jaloud.xyz>
2023-01-27 14:54:24 -0800

    Updated main info on webpage.

---
+++ mutarjim.py
@@ -0,0 +1,20 @@
+# tests = [
+#     ["صَالِحٌ", "ṣāliḥun"],
+#     ["النَّار", "al-nār"],
+#     ["الحال", "al-ḥal"],
+#     ["ضِيَاء", "ḍiyāʾ"],
+#     ["المَرأَة", "al-marʾah"],
+#     ["يَس", "yāsīn"]
+# ]
+
+
+# print(transliterate("يَس"))
+# print(transliterate("النَّار"))
+# print(transliterate("الحال"))
+# print(transliterate("ضِيَاء"))
+# print(transliterate("المَرأَة"))
+
+# for test in tests:
+#         result = transliterate(test[0])
+#         print(result, "/", test[1])
+#         assert result == test[1]
