796bdafd62ea9a54b2b20d6dca28ce96999aa258
Jaloud <github@jaloud.xyz>
2023-01-26 22:36:03 -0800

    Converted script to a web application using Flask and added a user input form and processing route.

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
+from flask import Flask, request, render_template
 import re
-tests = [
-    ["صَالِحٌ", "ṣāliḥun"],
-    ["النَّار", "al-nār"],
-    ["الحال", "al-ḥal"],
-    ["ضِيَاء", "ḍiyāʾ"],
-    ["المَرأَة", "al-marʾah"],
-    ["يَس", "yāsīn"]
-]
+import json
 
 def transliterate(arabic_text):
     rules = {
@@ -77,17 +71,13 @@ def transliterate(arabic_text):
     arabic_text = re.sub("ط?َه", "ṭāha", arabic_text)
     #مشكلة يس
     arabic_text = re.sub("(^|) يَس (.)", "yāsīn", arabic_text)
-
     arabic_text = re.sub("الرَّحمَ?ن", "al-raḥmān", arabic_text)
-
     # sunny AL الشمسية
     arabic_text = re.sub("(^| )ال(.)ّ", "\g<1>al-\g<2>", arabic_text)
-
     # moony AL القمرية
     arabic_text = re.sub("(^| )ال", "\g<1>al-", arabic_text)
     # مشكلة أحمد
     arabic_text = re.sub("(^| )أَ", "\g<1>a", arabic_text)
-
     # yaa مشكلة ضياء
     arabic_text = re.sub("يَاء", "yāʾ", arabic_text)
     # مشكلة مواساة
@@ -95,45 +85,39 @@ def transliterate(arabic_text):
     # مشكلة ابن تيمية
     arabic_text = re.sub("يَّ", "yya", arabic_text)
     # مشكلة واعتصموا
-
-
-
+    
     for i, char in enumerate(arabic_text):
-
-
         if prev_char + char in rules:
             transliteration = transliteration[:-1]
-
             transliteration += rules[prev_char + char]
-
             prev_char = char
-
         elif char in rules:
             transliteration += rules[char]
-
             prev_char = char
-
         elif char == "\u0651":
             transliteration += rules[prev_char]
-
         else:
             transliteration += char
             prev_char = char
-
+            
     return transliteration
 
 
-user_input = input('Enter text: ')
-print(transliterate(user_input))
+
+from flask import Flask, request
+
+app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+  
+@app.route("/process", methods=["POST"])
+def process():
+    user_input = request.form["user_input"]
+    result = transliterate(user_input)
+    return result
 
 
-# print(transliterate("يَس"))
-# print(transliterate("النَّار"))
-# print(transliterate("الحال"))
-# print(transliterate("ضِيَاء"))
-# print(transliterate("المَرأَة"))
-
-# for test in tests:
-#         result = transliterate(test[0])
-#         print(result, "/", test[1])
-#         assert result == test[1]
+if __name__ == '__main__':
+    app.run(debug=True)
